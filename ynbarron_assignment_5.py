9dd7622a043145adbfb06f0aef452649e0811be0
ynbarron-jpg <ynbarron@aggies.ncat.edu>
2025-09-30 18:22:38 -0400

    Add files via upload
    Challenge 2: Prime checker - for loop for known range

--- ynbarron_assignment_5.py
+++ ynbarron_assignment_5.py
@@ -13,3 +13,19 @@ while current_number != 1:
 print(1)
 print("Steps:", step_count)
 print() 
+
+print("=== Challenge 2: Prime Number Checker ===")
+current_number = int(input("Enter a number: "))
+print(f"Testing divisors from 2 to {current_number - 1}...")
+
+Int = 2
+while Int < current_number:
+    if current_number % Int == 0:
+        print(current_number, "is not prime!")
+        print("(divisible by", Int, ")")
+        break
+    Int = Int + 1
+
+if Int == current_number and current_number > 1:
+    print(current_number, "is prime!")
+print()
